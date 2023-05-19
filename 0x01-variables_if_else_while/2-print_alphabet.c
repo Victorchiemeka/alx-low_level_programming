70ef550381d4d262bb2e9887ba7bffc948037961
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-19 13:08:13 +0100

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function that prints only 
+ * Return: always sucess
+ */
+int main(void)
+{
+	//char c = 'a';
+	
+	for(int i =0; i<25; i++)
+	{
+		printf("%d",i);
+	}
+}
