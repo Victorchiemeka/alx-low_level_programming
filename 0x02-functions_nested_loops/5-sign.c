56e2be75c5065782961bdf4f5651042990f01395
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-23 14:20:12 +0100

    a function that prints the sign of a number.
    Prototype: int print_sign(int n);
    Returns 1 and prints + if n is greater than zero
    Returns 0 and prints 0 if n is zero
    Returns -1 and prints - if n is less than zero

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,21 +8,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
-		_putchar('1');
+		return (1);
 	}
 	else if (n == 0)
 	{
+		_putchar(48);
 		return (0);
-		_putchar(48);
-		_putchar(48);
 	}
 	else if (n < 0)
 	{
-		return (-1);
 		_putchar('-');
-		_putchar('1');
+		return (-1);
 	}
 	return (0);
 }
