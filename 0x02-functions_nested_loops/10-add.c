09d3f6480148abd4fb0cecdb62936b08ac4d8b03
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-23 15:58:45 +0100

    a function that adds two integers and returns the result.
    Prototype: int add(int, int);

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - a function that adds two
+ * @int: the type of the returned value
+ * Return: the value of compute
+ */
+int add(int, int)
+{
+	int a;
+	int b;
+	int compute;
+
+	compute = a + b;
+	return (compute);
+}
