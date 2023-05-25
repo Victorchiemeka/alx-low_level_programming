a0167f12b8b0a4bee1249a80a9fd8eb947057d3f
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-25 09:47:48 +0100

    a function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - a function that returns capital letters
+ * @c: the parameter for the function
+ * Return: 1 if condition is or 0 if false:
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
