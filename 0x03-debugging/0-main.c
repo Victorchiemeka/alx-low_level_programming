133d3c1b558dc1c3b1bb91bbbe97f7e89517025f
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-25 14:49:50 +0100

    task done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
