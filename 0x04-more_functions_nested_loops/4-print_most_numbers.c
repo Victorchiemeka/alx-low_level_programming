6d40c4d13a31a8505fe7ed4768f5b7d0fc828964
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-25 23:12:29 +0100

    task done

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,15 @@
  */
 void print_most_numbers(void)
 {
-	int c;
+	char c;
 
 	while (c <= 9)
 	{
-		if (i != 2 && i != 4)
+		if (c != 2 && c != 4)
 		{
-			_putchar(c + '0');
+			_putchar(c);
 		}
-		i++;
+		c++;
 	}
 	_putchar('\n');
 }
