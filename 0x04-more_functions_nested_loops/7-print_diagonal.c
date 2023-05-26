6882e8380d101577e8c375338b5e77b4dad3c151
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-26 13:28:20 +0100

    task done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,13 +16,12 @@ void print_diagonal(int n)
 
 		for (i = 0; i < n; i++)
 		{
-			_putchar('\n');
 			for (j = 0; j < i; j++)
 			{
 				_putchar(' ');
 			}
 			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
