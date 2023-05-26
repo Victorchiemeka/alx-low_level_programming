98b722ae2c52ccff7c04e40f0e55f1b9c5116b76
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-26 13:09:32 +0100

    task done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - a function that prints diagonal
+ * @n: a parameter
+ * return: always sucess
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\n');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,5 +9,6 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 int _isdigit(int c);
+void print_diagonal(int n);
 
 #endif
