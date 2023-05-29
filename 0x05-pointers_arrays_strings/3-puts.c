c0ac73eb0b5a1393eaf4fa7ea28942f1f00195a8
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-29 16:31:16 +0100

    task done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - a function that prints out
+ * @str: a parameter to the function
+ * return: always suceess
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
