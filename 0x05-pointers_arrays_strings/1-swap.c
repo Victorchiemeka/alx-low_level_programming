1b51a4d587187aa0f3ad4c32d9ca7ce40674611c
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-29 11:49:22 +0100

    task done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * swap_int - a function that swaps value
+ * @a: parameter to the function
+ * @b: another paremeter
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*b = c;
+}
