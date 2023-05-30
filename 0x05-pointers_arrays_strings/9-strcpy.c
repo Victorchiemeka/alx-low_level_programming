2c3deabf3ca28fa80f145d0b5aa293565ecb5692
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-30 22:02:59 +0100

    task dome

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcpy - a function that copies string
+ * @dest: parameter to the fucnction
+ * @src: parameter to the function
+ * Return: returns dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
