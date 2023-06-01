63ba1bede96cf7f09c897cf1324e5da65285eafa
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-01 13:23:15 +0100

    task done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - a function that prints number
+ * @n: the parameter to the function
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 1000)
+	{
+		_putchar((n / 1000) + '0');
+		n %= 1000;
+	}
+	if (n >= 100)
+	{
+		_putchar((n / 100) + '0');
+		n %= 100;
+	}
+	if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+		n %= 10;
+	}
+	_putchar(n + '0');
+}
