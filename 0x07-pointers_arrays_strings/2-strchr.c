dd786d50e28f0a6b7f2bfb16edfa396743d3fbfa
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-05 13:44:07 +0100

    task done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strchr - a function that returns a pointer to it first occurance
+ * @s: the parameter of the function
+ * @c: another parameter to the function
+ * Return: always the function
+ */
+char *_strchr(char *s, char c)
+{
+	char * ptr;
+
+	ptr = strchr(s, c);
+	return (ptr);
+}
