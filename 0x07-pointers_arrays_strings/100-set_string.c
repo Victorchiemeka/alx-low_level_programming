09e19860dd8e1e8d9aabc85c047cf2fb5bd6b1b2
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-05 20:14:30 +0100

    task done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - a function that sets string
+ * @s: the double pointer parameter
+ * @to: the second pointer
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
