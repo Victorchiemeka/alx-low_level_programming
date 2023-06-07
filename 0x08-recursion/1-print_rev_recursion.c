7a957e0f4f42e884f3440c8f918eb2aab1399d4e
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-07 09:53:15 +0100

    task done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int length, i;
-
-	length = strlen(s);
-	for (i = length - 1; i >= 0; i--)
+	if (*s)
 	{
-		_putchar(s[i]);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar('\n');
 }
