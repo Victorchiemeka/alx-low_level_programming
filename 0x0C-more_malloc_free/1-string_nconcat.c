225bc83257ac2f6c45ab85f673c6e188bf6f4524
VIctorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-15 12:44:50 +0100

    task done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - a function thar concatenates str
+ * @s1: first string
+ * @s2: second string
+ * @n: the number
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *all;
+	unsigned int i, length1, leng
+	if(s1 == NULL)
+		s1 = "";
+	if(s2 == NULL)
+		s2 = "";
+
+
+}
