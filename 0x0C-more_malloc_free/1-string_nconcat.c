07a4a3d4e75013f4cdd7f201f856d6ca008b8a06
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-15 13:01:21 +0100

    task done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * string_nconcat - a function thar concatenates str
  * @s1: first string
  * @s2: second string
  * @n: the number
+ * Return: the pointer 
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *all;
-	unsigned int i, length1, leng
-	if(s1 == NULL)
-		s1 = "";
-	if(s2 == NULL)
-		s2 = "";
+	unsigned int i, k, length1, leng
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	length1 = strlen(s1);
+	leng = strlen(s2);
+	if (n < leng)
+		all = malloc(sizeof(char) * (length1 + n + 1));
+	else
+		all = malloc(sizeof(char) * (length1 + leng + 1));
+	if (all == NULL)
+		return (NULL);
+	while (n < leng && i < (length1 + n))
+		whole[i++] = s2[j++];
+	while (n >= leng && i < (length1 + leng))
+		all[i++] = s2[j++];
+	all[i] = '\0';
+	return (all);
 
 }
