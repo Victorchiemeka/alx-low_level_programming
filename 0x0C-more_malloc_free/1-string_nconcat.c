7fccb8ab672ea7b789a9460926d330fda9231cff
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-15 13:15:21 +0100

    task done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,14 +17,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	length1 = strlen(s1);
-	leng = strlen(s2);
+	for (length1 = 0; s1[length1] != '\0'; length1++);
+	for (leng = 0; s2[leng] != '\0'; leng++);
+
 	if (n < leng)
 		all = malloc(sizeof(char) * (length1 + n + 1));
 	else
 		all = malloc(sizeof(char) * (length1 + leng + 1));
 	if (all == NULL)
 		return (NULL);
+	i = 0;
+	k = 0;
 	while (n < leng && i < (length1 + n))
 		all[i++] = s2[k++];
 	while (n >= leng && i < (length1 + leng))
