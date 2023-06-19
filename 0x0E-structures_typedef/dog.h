7aaaafb8569975c0e423ffffed3bda5851cda9ec
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-19 14:29:20 +0100

    task done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - the structure for the dog data
+ * @name: Is the name of the dog
+ * @age: Its age
+ * @owner: The owner of the dog
+ *
+ * Description: All these data are for the dog
+ */
+
 struct dog
 {
 	char *name;
