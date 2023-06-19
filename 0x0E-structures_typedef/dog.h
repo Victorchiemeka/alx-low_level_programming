efd896e2d463e7996f3b9c2d80fae6bf13be2371
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-19 15:28:46 +0100

    task done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,6 @@
  * Description: All these data are for the dog
  */
 
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog {
@@ -23,5 +17,6 @@ typedef struct dog {
     float age;
     char *owner;
 } dog_t;
+typedef struct dog dog_t;
 
 #endif
