6e879dcbc69084b53a180e83c56f4289e7fa44ba
Victorchiemeka <Victorchukwuemeka127@gmail.com>
2023-07-14 17:05:57 +0100

    task done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,25 @@
 #include "main.h"
-
 /**
- * create_file - creates a file and writes a given text content to it
- * @filename: the name of the file to create
- * @text_content: the NULL-terminated string to write to the file
+ * append_text_to_file - Append text to the end of a file
+ * @filename: name of file
+ * @text_content: string to add to end of file
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len = 0, res;
-
-	/* Check if filename argument is NULL */
+	int fd, status, i;
 	if (filename == NULL)
 		return (-1);
-
-	/* Open file for writing, create if it doesn't exist, truncate if it does */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (text_content == NULL)
+		return (1);
+	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-
-	/* Check if text_content argument is NULL */
-	if (text_content != NULL)
-	{
-		/* Calculate the length of the text content */
-		while (text_content[len])
-			len++;
-
-		/* Write the text content to the file */
-		res = write(fd, text_content, len);
-		if (res == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	status = write(fd, text_content, i);
+	if (status == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
