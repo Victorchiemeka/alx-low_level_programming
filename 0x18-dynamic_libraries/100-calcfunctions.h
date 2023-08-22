f84554a0f3b1370c68e45cf1990c09d91dd998d2
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-08-22 17:52:29 +0100

    dynamic lib done

---
+++ 0x18-dynamic_libraries/100-calcfunctions.h
@@ -0,0 +1,11 @@
+#ifndef CALCFUNCTION_H
+#define CALCFUNCTION_H
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+#endif /* CALCFUNCTION_H */
